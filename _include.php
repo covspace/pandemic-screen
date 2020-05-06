79ab66b51e6bd228e00c86e78fbd41f936339052
ssitaru <s.sitaru@gmx.de>
2020-05-06 15:36:58 +0200

    fix include

--- _include.php
+++ _include.php
@@ -1,5 +1,6 @@
 <?php
-include 'config.php';
+require_once dirname(__FILE__).'/config.php';
+require_once dirname(__FILE__).'/vendor/autoload.php';
 
 $templates = new League\Plates\Engine('templates');
 
