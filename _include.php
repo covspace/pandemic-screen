65f379fad39e341000dfc66ab6dfad46f1dd39d8
ssitaru <s.sitaru@gmx.de>
2020-05-06 15:39:36 +0200

    fix auto import

--- _include.php
+++ _include.php
@@ -1,6 +1,7 @@
 <?php
-require_once dirname(__FILE__).'/config.php';
 require_once dirname(__FILE__).'/vendor/autoload.php';
+require_once dirname(__FILE__).'/config.php';
+
 
 $templates = new League\Plates\Engine('templates');
 
--- import/automatic_import.php
+++ import/automatic_import.php
@@ -3,7 +3,6 @@
  * Automatic data import
  * 06.05.2020 Sebastian Sitaru
  */
-require_once dirname(__FILE__).'/../'.'_include.php';
 
 $importScripts = glob('*/import.py');
 
