af4be8176fdbf0cf10c09b32da460fd0c9c6f86e
ssitaru <s.sitaru@gmx.de>
2020-05-06 18:57:39 +0200

    fix csse import script

--- import/cssedb/import.py
+++ import/cssedb/import.py
@@ -7,7 +7,7 @@ import urllib2
 import os
 
 # dl
-todayDt = datetime.date.today().replace(day=datetime.date.today().day-1)
+todayDt = datetime.datetime.today().replace(day=datetime.date.today().day-1)
 today = todayDt.strftime('%m-%d-%Y')
 url = 'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/'+today+'.csv'
 f = urllib2.urlopen(url)
