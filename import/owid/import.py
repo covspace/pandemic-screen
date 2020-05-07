75bab5c76a85e0b68b2bf8c09baf8605d1ca015d
ssitaru <s.sitaru@gmx.de>
2020-05-07 16:23:57 +0200

    owid auto import script ok

--- import/owid/import.py
+++ import/owid/import.py
@@ -37,7 +37,7 @@ with open('owid-covid-data.csv') as csvfile:
 			# "total_deaths_per_million", "new_deaths_per_million", "total_tests", "new_tests",
 			# "total_tests_per_thousand","new_tests_per_thousand", "tests_units" ],
 			# only import most recent date
-			if(datetime.datetime.strptime(row['date'], '%Y-%m-%d') != todayDt):
+			if(row['date'] != todayDt.strftime('%Y-%m-%d')):
 				continue
 
 			# first put in -1 for no data
@@ -49,10 +49,10 @@ with open('owid-covid-data.csv') as csvfile:
 				'total_cases': int(row['total_cases']), 'new_cases': int(row['new_cases']), 'total_deaths': int(row['total_deaths']),
 				'new_deaths': int(row['new_deaths']), 'total_cases_per_million': float(row['total_cases_per_million']), 'new_cases_per_million': float(row['new_cases_per_million']),
 				'total_deaths_per_million': float(row['total_deaths_per_million']), 'new_deaths_per_million': float(row['new_deaths_per_million']),
-				'total_tests': int(row['total_tests']), 'new_tests': int(row['new_tests']), 'total_tests_per_thousand': float(row['total_tests_per_thousand']),
+				'total_tests': float(row['total_tests']), 'new_tests': float(row['new_tests']), 'total_tests_per_thousand': float(row['total_tests_per_thousand']),
 				'new_tests_per_thousand': float(row['new_tests_per_thousand']), 'tests_units': str(row['tests_units']) }
 			print dict
-			#x = owid.insert_one(dict)
-			print x
+			x = owid.insert_one(dict)
+			#print x
 
 os.remove('owid-covid-data.csv')
