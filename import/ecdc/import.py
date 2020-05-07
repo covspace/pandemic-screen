5584c73e33bb8f179d8ee4f654ca539c344a35c8
ssitaru <s.sitaru@gmx.de>
2020-05-07 16:40:00 +0200

    ecdc auto import script

--- import/ecdc/import.py
+++ import/ecdc/import.py
@@ -4,17 +4,29 @@
 import csv
 import pymongo
 import datetime
+import urllib2
+import os
 
+response = urllib2.urlopen('https://opendata.ecdc.europa.eu/covid19/casedistribution/csv')
+data = response.read()
+filename = "data.csv"
+file = open(filename, 'wb')
+file.write(data)
+file.close()
+todayDt = datetime.datetime.today()
 
 myclient = pymongo.MongoClient("mongodb://admin:covid19season@localhost:27017")
 mydb = myclient["covspace"]
 countriesdb = mydb["countries"]
 owid = mydb["eurostat-population"]
 
-with open(today+'.csv') as csvfile:
+with open('data.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
 		# dateRep,day,month,year,cases,deaths,countriesAndTerritories,geoId,countryterritoryCode,popData2018,continentExp
+		if(row['dateRep'] != todayDt.strftime('%d/%m/%Y')):
+			continue
+
 		country = countriesdb.find_one({'iso_code': row['geoId']})
 		if country:
 			#
@@ -30,4 +42,4 @@ with open(today+'.csv') as csvfile:
 
 			print "inserted ds for", country['iso_code'], "and", row['dateRep']
 
-os.remove(today+'.csv')
+os.remove('data.csv')
