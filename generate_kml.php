9f34569fb9bfe1633fa4b6cdec82cfdd2c89253d
ssitaru <s.sitaru@gmx.de>
2020-05-07 20:08:33 +0200

    test kml

---
+++ generate_kml.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);
+
+require 'vendor/autoload.php';
+require '_include.php';
+
+header('Content-type: application/vnd.google-earth');
+
+?>
+
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+  <Placemark>
+    <name>Simple placemark</name>
+    <description>Attached to the ground. Intelligently places itself
+       at the height of the underlying terrain.</description>
+    <Point>
+      <coordinates>-122.0822035425683,37.42228990140251,0</coordinates>
+    </Point>
+  </Placemark>
+</kml>
